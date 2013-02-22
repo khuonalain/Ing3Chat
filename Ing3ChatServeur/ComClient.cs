05c269b7b3f03444caf85183623e3d0f7641c900
Alain khuon <khuonalain@gmail.com>
2013-02-22 10:08:04 +0100

    gestion de la socket

---
+++ Ing3ChatServeur/ComClient.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Ing3ChatServeur
+{
+    class ComClient
+    {
+        private string pseudo;
+
+        public string Pseudo
+        {
+            get { return pseudo; }
+            set { pseudo = value; }
+        }
+
+        private Socket socketCom;
+
+        public Socket SocketCom
+        {
+            get { return socketCom; }
+            set { socketCom = value; }
+        }
+
+        public ComClient(Socket s)
+        {
+            this.socketCom = s;
+        }
+    }
+}
--- Ing3ChatServeur/Program.cs
+++ Ing3ChatServeur/Program.cs
@@ -2,15 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
 
 namespace Ing3ChatServeur
 {
     class Program
     {
+        private static List<ComClient> clients = new List<ComClient>();
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Démarrage du serveur");
+            //AddressFamily.InterNetwork = IPV4
+            //SocketType.Stream = type de donnée à recevoir, ici c'est que de la data (envoi de donnée apres acceptation de la demande), pour se connecter à un serveur web, on utilise le raw (envoi de donnée en meme tant que la demande)
+            Socket sockServeur = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
+            IPHostEntry cartes = Dns.GetHostEntry("127.0.0.1");
+            foreach (IPAddress ip in cartes.AddressList)
+            {
+                Console.WriteLine(ip.ToString()+"\n");
+            }
 
+            IPEndPoint iep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 2013);
+
+            Console.ReadLine();
         }
     }
 }
